5d34c2ec03a303c38b0271cdc42fca2869113cad
jhenriquecampos <jhcc1995@gmail.com>
2016-08-19 02:57:21 -0300

    Update Questão 4.c

--- Questão 4.c
+++ Questão 4.c
@@ -1,3 +1,15 @@
+//4. 	Faça um algoritmo, em C, que leia dois vetores A e B de tamanho 10. Depois gere, a partir deles, um vetor C, o qual
+//	corresponde a intercalação dos elementos dos vetores A e B. Ou seja, você deve colocar em C um elemento de A,
+//	depois um elemento de B, outro elemento de A, outro elemento de B e assim sucessivamente. Todos os elementos de
+//	A e B devem estar em C.
+//	Exemplo:
+//	A:
+//	5 2 3 4 1 6 9 8 7 10
+//	B:
+//	18 15 13 14 12 16 17 11 19 20
+//	C:
+//	5 18 2 15 3 13 4 14 1 12 6 16 9 17 8 11 7 19 10 20
+
 #include <stdio.h>
 #include <locale.h>
 #define tam 20
@@ -7,7 +19,7 @@ void preencherVINT(int vet[])//preenchimento dos vetores de inteiros
 	int i;
 	for (i = 0; i<tam; i++)
 	{
-		printf("N�mero %d: ", i+1);
+		printf("Número %d: ", i+1);
 		scanf("%d", &vet[i]);
 	}
 	printf("\n");
